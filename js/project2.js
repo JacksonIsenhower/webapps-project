415fb78be77d742b7e9b7e6b7ecb15661cc906b9
8mcgraw <123113246+8mcgraw@users.noreply.github.com>
2023-02-14 02:55:30 -0500

    Added Search JS

--- js/project2.js
+++ js/project2.js
@@ -23,3 +23,51 @@ function Term(semester, courses = []) {
 	this.semester = semester;
 	this.courses = courses;
 };
+
+
+function searchCourses(){
+	let notValid = /[^\w" "+-]/;
+	let amountOfCourses = 0;
+	let num = document.getElementById("showing");
+	let input = document.getElementById("search").value.toUpperCase();
+	let table = document.getElementById("courselist");
+	for (let i = 1, cell; cell = table.rows[i]; i++) {
+		let t = cell.textContent || cell.innerText;
+		if (t.toUpperCase().indexOf(input)>-1){
+			cell.style.display="";
+			amountOfCourses+=1;
+		}else{ 
+			cell.style.display="none";
+		}
+	}
+	num.innerHTML = ("Showing 1 to "+amountOfCourses +" of "+amountOfCourses);
+	num.style.color = "black";
+	if (amountOfCourses==0){
+		num.innerHTML = ("No Classes Found");
+		num.style.color = "blue";
+	}
+	if (input.match(notValid)){
+		num.innerHTML = ("Invalid input");
+		num.style.color = "red";
+}
+
+function clearCourses(){
+	document.getElementById("search").value = "";
+	searchCourses();
+}
+
+window.onload = init;
+function init(){
+	let amountOfCourses = document.getElementById("courselist").rows.length-1
+	let num = document.getElementById("showing");
+	num.innerHTML = ("Showing 1 to "+amountOfCourses +" of "+amountOfCourses);
+	if (amountOfCourses==0){
+		num.innerHTML = ("No Classes Found");
+		num.style.color = "blue";
+	}
+	document.getElementById("search").addEventListener("keyup", searchCourses);
+	document.getElementById("clear").addEventListener("click", clearCourses);
+	document.getElementById("clear").style.cursor = "pointer";
+}
+
+
