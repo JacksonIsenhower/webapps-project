ebe810c012210e59b596007935f31d00dfb27267
Jackson Isenhower <jacksonisenhower@cedarville.edu>
2023-02-13 20:02:41 -0500

    Added Course object constructor to JS file.

--- js/project2.js
+++ js/project2.js
@@ -6,3 +6,11 @@ let javascriptPlan = {
 	currentSemester: "",
 	courses: []
 };
+
+function Course(id, name, credits, term, year) {
+	this.id = id;
+	this.name = name;
+	this.credits = credits;
+	this.term = term;
+	this.year = year;
+}
