201bf55cfda1b387fdac4b9e6ba5ff99095fb803
Jackson Isenhower <jackson@isenhower.net>
2023-03-21 08:15:24 -0400

    Started attempting global plan var, but ran into implementation issues.

--- php/project4_getCombined.php
+++ php/project4_getCombined.php
@@ -14,10 +14,10 @@ function getCurrentTerm() {
 
 session_start();
 
-$DATABASE_HOST = 'james.cedarville.edu';
-$DATABASE_USER = 'cs3220_sp23';
-$DATABASE_PASS = 'E57y6Z1FwAlraEmA';
-$DATABASE_NAME = 'cs3220_sp23';
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'test';
 
 if (!isset($_SESSION['id'])) {
 	exit('No user provided');
@@ -29,33 +29,30 @@ if ( mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL:' . mysqli_connect_error());
 }
 
-$combined = array();
-$plans = array();
+$combined = array("5");
+$plan = array();
 $catalogs = array();
 
 // Hit the database tables to get the Plan information
-if ($stmt = $con->prepare('SELECT iaj_user.name, iaj_plan.plan_id, iaj_plan.plan_name, iaj_plan.catalog, iaj_plan.default_plan FROM iaj_user, iaj_plan WHERE iaj_user.ID = iaj_plan.user_id AND iaj_user.ID = ?')) {
-	$stmt->bind_param('s', $_SESSION['id']);
+if ($stmt = $con->prepare('SELECT iaj_user.name, iaj_plan.plan_id, iaj_plan.plan_name, iaj_plan.catalog, iaj_plan.default_plan FROM iaj_user, iaj_plan WHERE iaj_user.ID = iaj_plan.user_id AND iaj_user.ID = ? AND iaj_plan.plan_id = ?')) {
+	$stmt->bind_param('ss', $_SESSION['id'], $_SESSION['plan']);
 	$stmt->execute();
 	$stmt->store_result();
 
 	if ($stmt->num_rows > 0) {
 		$stmt->bind_result($userName, $planID, $planName, $planCatalog, $planDefault);
-		while ($row = $stmt->fetch()) {
-			$plans[$planID] = array("name"=>$planName,"student"=>$userName,"catalog"=>$planCatalog,"default"=>$planDefault,"courses"=>array(),"major"=>"TEMP_MAJOR","currYear"=>(int)date("Y"),"currTerm"=>getCurrentTerm());
-			
-			if ($courseStmt = $con->prepare('SELECT course_id, year, term FROM iaj_plan_courses WHERE plan_id = ?')) {
-				$courseStmt->bind_param('s', $planID);
-				$courseStmt->execute();
-				$courseStmt->store_result();
-				while ($courseRow = $courseStmt->fetch()) {
-					$courseStmt->bind_result($courseID, $courseYear, $courseTerm);
-					$plans[$planID]["courses"][$courseID] = array("id"=>$courseID,"year"=>$courseYear,"term"=>$courseTerm);
-				}
+		$plan = array("name"=>$planName,"student"=>$userName,"catalog"=>$planCatalog,"default"=>$planDefault,"courses"=>array(),"major"=>"TEMP_MAJOR","currYear"=>(int)date("Y"),"currTerm"=>getCurrentTerm());
+		if ($courseStmt = $con->prepare('SELECT course_id, year, term FROM iaj_plan_courses WHERE plan_id = ?')) {
+			$courseStmt->bind_param('s', $planID);
+			$courseStmt->execute();
+			$courseStmt->store_result();
+			$courseStmt->bind_result($courseID, $courseYear, $courseTerm);
+			while ($courseRow = $courseStmt->fetch()) {
+				$plan["courses"][$courseID] = array("id"=>$courseID,"year"=>$courseYear,"term"=>$courseTerm);
 			}
 		}
 	}
-	$combined["plans"] = $plans;
+	$combined["plan"] = $plan;
 }
 
 // Hit the database tables to get the Catalog information
@@ -72,8 +69,8 @@ if ($stmt = $con->prepare('SELECT iaj_catalog.year FROM iaj_catalog')) {
 				$courseStmt->bind_param('s', $catalog);
 				$courseStmt->execute();
 				$courseStmt->store_result();
+				$courseStmt->bind_result($courseID, $courseName, $courseDescription, $courseCredits);
 				while ($courseRow = $courseStmt->fetch()) {
-					$courseStmt->bind_result($courseID, $courseName, $courseDescription, $courseCredits);
 					if ($courseID != null) {
 						$catalogs[$catalog]["courses"][$courseID] = array("id"=>$courseID,"name"=>$courseName,"description"=>$courseDescription,"credits"=>$courseCredits);
 					}
